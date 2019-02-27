2cf76c3c4514e6c51224e9032c501fff3f572e9d
329731984@qq.com <329731984@qq.com>
2019-02-27 16:57:25 +0800

    add

---
+++ src/TestTime.java
@@ -0,0 +1,28 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author: yan
+ * @Date: 2018/6/16/0016 9:30
+ * @Description:
+ */
+public class TestTime {
+
+
+    /**
+     * 把long转成date 用calendar计算再换成string
+     * @param args
+     */
+    public static void main(String args[]) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        long longTime = new Date().getTime();
+        System.out.println("long为：" + longTime);
+        System.out.println("Long转换成日期:");
+        System.out.println("longToDate：" + new SimpleDateFormat("yyyy-MM-dd ").format(new Date(longTime)));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(longTime));
+        calendar.add(Calendar.DAY_OF_MONTH, +75);
+        System.out.println("75天后：" + sdf.format(calendar.getTime().getTime()));
+    }
+}
--- src/demo.java
+++ src/demo.java
@@ -20,7 +20,24 @@ public class demo {
                  }
              }
 
+             for (int num :param){
+                 System.out.println(num+"");
          }
+         }
+
+//    private static int getValue(int i){
+//        int result =0;
+//        switch (i){
+//            case 1:
+//                result = result+i;
+//            case 2:
+//                result = result+i*2;
+//            case 3:
+//                result= result +i*3;
+//        }
+//        System.out.println(result);
+//        return result;
+//    }
 }
 
 
