0cb6aaa32774bac5256388e8372b918718b4b782
329731984@qq.com <329731984@qq.com>
2019-03-16 23:29:34 +0800

    update

---
+++ src/Example/StringTest.java
@@ -0,0 +1,23 @@
+package Example;
+
+/**
+ * Created by Ryan on 2019/3/16/0016
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+
+//        String s1 ="Please_input_string";
+//        String resultstring = s1.substring(7);
+//        System.out.println(resultstring);
+//        String s2 =resultstring.substring(0,5);
+//        System.out.println(s2);
+//        System.out.println(s2+":"+s1);
+
+        int a =5;
+        int b =2;
+        System.out.println(a/b);
+        double a1 =a/b;
+        System.out.println(a1);
+    }
+}
