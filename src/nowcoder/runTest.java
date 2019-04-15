88dfd9bc810b373eaaef1a0dc53a4e4ab93276a0
329731984@qq.com <329731984@qq.com>
2019-04-15 23:10:12 +0800

    update

---
+++ src/nowcoder/runTest.java
@@ -0,0 +1,20 @@
+package nowcoder;
+
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+/**
+ * Created by Ryan on 2019/4/15/0015
+ */
+public class runTest {
+
+    public static void main(String[] args){
+
+            int sum =0;
+            for(int i=1;i<=5;i++){
+                sum += i;
+
+            }
+        System.out.println( sum) ;
+        }
+    }
+
