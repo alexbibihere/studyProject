cf0e6a854f09440a6a0698b6d76790c1a3b2c720
329731984@qq.com <329731984@qq.com>
2019-06-12 20:53:02 +0800

    update twoSum HashMap

--- src/Example/LeetCode/TwoSum.java
+++ src/Example/LeetCode/TwoSum.java
@@ -1,5 +1,9 @@
 package Example.LeetCode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: yan
  * @Date: 2019/5/26/0026 19:37
@@ -9,25 +13,65 @@ public class TwoSum {
 
     public static void main(String[] args) {
 //        public int[] twoSum(int[] nums, int target) {
-            int[] sum = {2,7,11,5};
-            int[] abb = {0,0};
+            int sum[] = {2,7,11,5};
+            int[] abb = new int[2];
             int target = 9;
 
             //如果要判断数组中两数之和等于目标值
 
-            for(int i=0;i<sum.length-1;i++){
-                for(int j=i+1;j<sum.length;j++){
-//                    int Su = ;
-                    if(sum[i]+sum[j] ==target){
-                        abb[0] =i;
-                        abb[1] =j;
-                        System.out.println("abb[0]:"+i);
-                        System.out.println("abb[1]:"+j);
-                    }
+
+            //穷举循环
+//            for(int i=0;i<sum.length-1;i++){
+//                for(int j=i+1;j<sum.length;j++){
+//                    if(sum[i]+sum[j] ==target){
+//                        abb[0] =i;
+//                        abb[1] =j;
+//                        System.out.println("abb[0]:"+i);
+//                        System.out.println("abb[1]:"+j);
+//                    }
+//                }
+//            }
+
+            //HashMap循环
+//            Map<Integer,Integer> map = new HashMap<>(sum.length);
+//            for(int i=0;i<sum.length;i++){
+//
+//                int value = target-sum[i];
+//                Integer index = map.get(value);
+//                if(index!=null && index!=i){
+//                    abb[0]=i;
+//                    abb[1]=index;
+//                    break;
+//                }
+//                map.put(abb[i],i);
+//                System.out.println("abb[i]:"+abb[i]+"-i:"+i);
+//            }
+
+
+        int[] ints = TwoSum.twoSum(sum, target);
+        System.out.println(Arrays.toString(ints));
+    }
+
+
+        public static int[] twoSum(int[] nums, int target){
+
+            int[] sum = {2,7,11,5};
+            int[] abb = new int[2];
+
+
+            Map<Integer,Integer> map = new HashMap<>(sum.length);
+            for(int i=0;i<sum.length;i++){
+
+                int value = target-sum[i];
+                Integer index = map.get(value);
+                if(index!=null && index!=i){
+                    abb[0]=i;
+                    abb[1]=index;
+                    break;
                 }
+                map.put(sum[i],i);
+                System.out.println("sum[i]:"+sum[i]+"-i:"+i);
             }
-
-
+                return abb;
         }
-
     }
